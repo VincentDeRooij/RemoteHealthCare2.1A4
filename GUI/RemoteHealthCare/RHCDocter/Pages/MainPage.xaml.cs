2bdf0f802ec29a7b7fd63e37566aefd3f58fa97e
Lorenzo <lrstolk@avans.nl>
2019-10-09 16:01:06 +0200

    [Added] Added functionalities for Docter GUI to add objects.

--- GUI/RemoteHealthCare/RHCDocter/Pages/MainPage.xaml.cs
+++ GUI/RemoteHealthCare/RHCDocter/Pages/MainPage.xaml.cs
@@ -24,16 +24,32 @@ namespace RHCDocter.Pages
     {
         private List<MainWindow.Person> Persons; //TODO: get docter his clients 
         private List<MainWindow.Session> ArchivedSessions; //TODO: get archived sessions 
+        private bool userOnline; //aka currentSelectedUserIsOnline 
 
         public MainPage()
         {
             InitializeComponent();
+            InitSettings();
+        }
+
+        private void InitSettings()
+        {
             ClientsListBox.SelectionMode = SelectionMode.Single;
             ArchivedSessionsListBox.SelectionMode = SelectionMode.Single;
+
+            userOnline = false;
+            //AddMessageToView(true, "DokterMessage");
+            //AddMessageToView(false, "ClientMSG");
+
+            if (ArchivedSessionsListBox.SelectedIndex < 0)
+            {
+                BTNConfirm.IsEnabled = false;
+            }
         }
 
         private void Button_Click_Send(object sender, RoutedEventArgs e)
         {
+            String message = TXTBoxMessageSend.Text;
 
         }
 
@@ -51,21 +67,52 @@ namespace RHCDocter.Pages
 
         private void Button_Click_Confirm(object sender, RoutedEventArgs e)
         {
+            int index = ArchivedSessionsListBox.SelectedIndex;
+            Console.Out.WriteLine($"index: {index}");
 
+            if (ClientsListBox.SelectedIndex < 0)
+            {
+                MessageBox.Show("Select a user");
+            } else if (ArchivedSessionsListBox.SelectedIndex < 0)
+            {
+                MessageBox.Show("Select a archived session");
+            }
+            else
+            {
+                SessionWindow sw = new SessionWindow(ClientsListBox.SelectedItem.ToString().Remove(0, 37));
+                sw.Show();
+            }
         }
 
         private void LB_Clients_SelectChanged(object sender, RoutedEventArgs e)
         {
             int index = ClientsListBox.SelectedIndex;
+            //TODO: check if user is online, and set bool 'userOnline' 
             //Console.Out.WriteLine(index);
+            ClientUserName.Content = ClientsListBox.SelectedItem.ToString().Remove(0, 37);
 
+            if (userOnline)
+            {
+                ClientUserStatus.Content = "Online";
+                BTNSend.IsEnabled = true;
+                BTNCreate.IsEnabled = true;
+            }
+            else
+            {
+                ClientUserStatus.Content = "Offline";
+                BTNSend.IsEnabled = false;
+                BTNCreate.IsEnabled = false;
+            }
         }
-
-
 
         private void LB_ArchivedSessions_SelectChanged(object sender, RoutedEventArgs e)
         {
             int index = ArchivedSessionsListBox.SelectedIndex;
+            if (index >= 0)
+            {
+                BTNConfirm.IsEnabled = true;
+            }
+
             //Console.Out.WriteLine(index);
         }
 
@@ -112,6 +159,65 @@ namespace RHCDocter.Pages
                 TXTBoxTimeSession.Text = result;
                 TXTBoxTimeSession.CaretIndex = result.Length;
             }
+        }
+
+        private void AddArchivedSessionToView(String sessionTitle)
+        {
+            //TODO: add GUI layout for archived session 
+            ListBoxItem lbi = new ListBoxItem();
+            lbi.HorizontalContentAlignment = HorizontalAlignment.Left;
+            lbi.Height = 30;
+            lbi.Width = 260;
+            lbi.Background = Brushes.DarkGray;
+            lbi.Padding = new Thickness(5);
+            lbi.Content = sessionTitle;
+
+            ArchivedSessionsListBox.Items.Add(lbi);
+        }
+
+        private void AddPersonToView(String clientUserName)
+        {
+            //TODO: add GUI layout per person 
+            ListBoxItem lbi = new ListBoxItem();
+            lbi.HorizontalContentAlignment = HorizontalAlignment.Left;
+            lbi.Height = 30;
+            lbi.Width = 260;
+            lbi.Background = Brushes.LightGray;
+            lbi.Padding = new Thickness(5);
+            lbi.Content = clientUserName;
+
+            ClientsListBox.Items.Add(lbi);
+        }
+
+        private void AddMessageToView(bool isDokterMessage, string message)
+        {
+            Label lbl = new Label();
+            TextBlock txtb = new TextBlock();
+
+            txtb.TextWrapping = System.Windows.TextWrapping.WrapWithOverflow;
+            txtb.Padding = new Thickness(5);
+            txtb.Text = message;
+
+            lbl.MaxWidth = 250;
+            lbl.HorizontalAlignment = HorizontalAlignment.Left;
+            
+            lbl.BorderThickness = new Thickness(1);
+            lbl.BorderBrush = Brushes.DarkGray;
+            lbl.Margin = new Thickness(0, 5, 0, 5);
+
+            if (isDokterMessage)
+            {
+                lbl.HorizontalAlignment = HorizontalAlignment.Right;
+                lbl.Background = Brushes.LightGray;
+            }
+            else
+            {
+                lbl.HorizontalAlignment = HorizontalAlignment.Left;
+                lbl.Background = Brushes.GhostWhite;
+            }
+                
+            lbl.Content = txtb;
+            MessagesPanel.Children.Add(lbl);
         }
     }
 }
---
+++ GUI/RemoteHealthCare/RHCDocter/SessionWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RHCDocter
+{
+    /// <summary>
+    /// Interaction logic for SessionWindow.xaml
+    /// </summary>
+    public partial class SessionWindow : Window
+    {
+        public SessionWindow(String clientUserName)
+        {
+            InitializeComponent();
+            Title = $"Session '{clientUserName}' at {DateTime.Today}";
+            SessionMainFrame.NavigationUIVisibility = NavigationUIVisibility.Hidden;
+
+            SessionMainFrame.CommandBindings.Add(new CommandBinding(NavigationCommands.BrowseForward, OnBrowseForward));
+            SessionMainFrame.CommandBindings.Add(new CommandBinding(NavigationCommands.BrowseBack, OnBrowseBack));
+
+            void OnBrowseForward(object sender, ExecutedRoutedEventArgs args)
+            {
+                Console.WriteLine($"Forward Mainframe");
+                //Do Nothing 
+            }
+
+            void OnBrowseBack(object sender, ExecutedRoutedEventArgs args)
+            {
+                Console.WriteLine($"Back Mainframe");
+                //Do Nothing 
+            }
+        }
+    }
+}
